a9d0a137f289950cce2f05255b8542047fb93e6e
itdevmasterteam <itdevmasterteam@gmail.com>
2022-01-28 14:34:08 +0530

    sign in page and sign up completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,109 @@
-import { Component } from '@angular/core';
-
+import { Component,ViewChild } from '@angular/core';
+import { FormGroup,FormBuilder, Validators,NgForm } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+
+   role = [
+    {id:1,name:'Employee'},
+    {id:2,name:'Manager'},
+    {id:3,name:'Ceo'},
+    {id:4,name:'Director'},
+
+   ]
+  
+  roleId;
+
+  // @ViewChild('formDirective') private formDirective: NgForm;
+  signupForm: FormGroup = new FormGroup({});
+  constructor(private fb: FormBuilder) { }
+  // signupForm:FormGroup
+ngOnInit() {
+  this.signupForm = this.fb.group({
+    firstName:['',[Validators.required]],
+     lastName: ['', [Validators.required, Validators.maxLength(8)]],
+     age: ['', [Validators.required]],
+      gender:['',[Validators.required]],
+     date: [new Date(2019,12,1), [Validators.required]],
+      role:[4,[Validators.required]],
+    password:['',[Validators.required]],
+     confirmPassword: ['', [Validators.required]],
+    
+     
+     
+     
+     
+  }, {
+      validator: () => {
+      // let valid
+      if (this.signupForm?.controls?.password?.value != this.signupForm?.controls?.confirmPassword?.value) {
+        // valid = false;
+        this.signupForm.controls.confirmPassword.setErrors({ validatePasswordConfirmation: true });
+        // }
+        console.log('this', this.signupForm)
+      // return valid;
+    }
+     }
+   })
+  
+}
+
+
+  hide = true;  
+  submitted = false;
+  signUpSubmitted = false;
+  loginForm = this.fb.group({
+    email:['',[Validators.required,Validators.email]],
+    password:['',[Validators.required,Validators.maxLength(8)]],
+
+  })
+ 
+ 
+  
+  
+   get signUpControl() {
+     
+    return  this.signupForm.controls
+   }
+  
+
+  confirmPasswordChecker(group: FormGroup) {
+    // console.log('signUpform',group.get('confirmPassword'))
+    let valid = true;
+   
+      
+      
+    }
+
+  
+   get formControl() {
+    return  this.loginForm.controls
+   }
+  
+  
+  onSubmit(loginForm) {
+    this.submitted=true
+    console.log({loginForm})
+    console.log('dsada',this.formControl)
+  }
+
+  signUpSubmit() {
+    this.signUpSubmitted=true
+  }
+
+  tabClicked() {
+    console.log('hi',this.roleId)
+    // this.resetForm()
+    // this.formDirective.resetForm();
+  }
+//   resetForm() {
+//  }
+  
+
+
   title = 'angular-auth';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,44 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {MatButtonModule} from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatTabsModule } from '@angular/material/tabs';
+import {MatCardModule} from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from "@angular/forms";
+import { MatIconModule } from '@angular/material/icon';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule, MatRippleModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import {MatTooltipModule} from '@angular/material/tooltip';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    MatTabsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatRadioModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatRippleModule,
+    MatSelectModule,
+ MatTooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
